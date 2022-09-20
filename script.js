f8f169c8bb14b2901003f57e02f10e0934bc7dfd
Dilsin Amirtharajah <KillerzDaggers@Outlook.com>
2022-09-20 17:21:43 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let firstName = "Dilsin";
+let lastName = " Amirtharajah";
+let age = 17;
+
+me = firstName + lastName;
+alert("My name is " + me);
+
+alert("I am " + age + " years old");
+
+console.log("I am having so much fun learning code in this class!")
